d14d1cc5b633df99ef2e2a1b0d71aafa21b9c47a
Abdullah Zameek <arz268@nyu.edu>
2020-05-21 19:45:56 +0400

    Updates

---
+++ precache-manifest.0525ffa6e979517fdba03d1605076343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e42087daafff60e29c851338f1f19",
+    "url": "/summarizer/index.html"
+  },
+  {
+    "revision": "15f7d32db5da0d8d2172",
+    "url": "/summarizer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fbeb234ec928b06db2dc",
+    "url": "/summarizer/static/js/2.3c355df0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/summarizer/static/js/2.3c355df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f7d32db5da0d8d2172",
+    "url": "/summarizer/static/js/main.656036ec.chunk.js"
+  },
+  {
+    "revision": "9c97186dac57b11b0e39",
+    "url": "/summarizer/static/js/runtime-main.83ebc216.js"
+  }
+]);
